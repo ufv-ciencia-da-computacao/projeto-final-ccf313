cd3fa70fc2bfeb602487dc944ee718f2c4f6a4c9
fabiotrindade <55442551+Fabio-Trindade@users.noreply.github.com>
2021-05-17 13:24:13 -0300

    Update ConnectionFactory.java
    path jdbc excluido

--- src/main/java/persistence/mysql/ConnectionFactory.java
+++ src/main/java/persistence/mysql/ConnectionFactory.java
@@ -7,9 +7,9 @@ import java.util.Properties;
 import java.util.logging.Logger;
 
 public class ConnectionFactory {
-    public static final String URL = "jdbc:mysql://localhost:3306/tpfinalpoo    ";
-    public static final String USER = "root";
-    public static final String PASS = "fabiodtnaf";
+    public static final String URL = "";
+    public static final String USER = "";
+    public static final String PASS = "";
 
     public static Connection getConnection() {
         try {
